6e293868d6e43a8e076c93cd82f6b931c674efbf
whobrien <whotemp@hotmail.com>
2022-03-08 01:02:28 -0500

    1.1 computerPlay finished

---
+++ rps.js
@@ -0,0 +1,14 @@
+function computerPlay() {
+    const computerNum = Math.floor(Math.random() * 3);
+    let computerSelection = '';
+
+    if (computerNum === 0) {
+        return computerSelection = 'Rock';
+    }
+    if else (computerNum === 1) {
+        return computerSelection = 'Paper';
+    }
+    else {
+        return computerSelection = 'Sciccors';
+    }
+}

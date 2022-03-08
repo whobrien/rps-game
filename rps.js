a8cd61c0931ca7a22b5f27703696d66c40c90375
whobrien <whotemp@hotmail.com>
2022-03-08 04:03:10 -0500

    2.0 Final with Alerts

--- rps.js
+++ rps.js
@@ -1,14 +1,33 @@
-function computerPlay() {
-    const computerNum = Math.floor(Math.random() * 3);
-    let computerSelection = '';
+let playerSelection = ''; //player selection var
+let computerSelection = ''; // create var for computer
+let computerRandom = ''; //create var for number Random
 
-    if (computerNum === 0) {
+function computerPlay() {  // Function to Return a random Rock, Paper, Scissor selection
+    computerRandom = Math.floor(Math.random() * 3); //Random int between 0-2
+
+    if (computerRandom === 0) {
         return computerSelection = 'Rock';
     }
-    if else (computerNum === 1) {
+    else if (computerRandom === 1) {
         return computerSelection = 'Paper';
     }
     else {
-        return computerSelection = 'Sciccors';
+        return computerSelection = 'Scissors';
     }
+}
+
+
+function playRound(playerSelection, computerSelection) {
+    computerSelection = computerPlay();
+
+    if (playerSelection === computerSelection) {
+        alert(`You Picked ${playerSelection}.  I picked ${computerSelection}. Push, Try Again!`);
+    }
+    else if (((computerSelection === 'Rock') && (playerSelection === 'Paper')) || ((computerSelection === 'Paper') && (playerSelection === 'Scissors')) || ((computerSelection === 'Scissors') && (playerSelection === 'Rock'))) {
+        alert(`You Picked ${playerSelection}.  I picked ${computerSelection}. You Win!`);
+    }
+    else {
+        alert(`You Picked ${playerSelection}.  I picked ${computerSelection}. Loser!`);
+    }
+
 }
